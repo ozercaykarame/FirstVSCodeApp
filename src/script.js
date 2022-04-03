be979919f1733a416de959b08b9b1c5afa637a6d
ozercaykara <ozer.caykara@me.com>
2022-04-03 18:56:20 +0300

    change js

--- src/script.js
+++ src/script.js
@@ -1,2 +1,9 @@
-console.clear();
-console.log("Hello World!");
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let numbersOdd = numbers.map(x => {
+    if (x % 2 === 1) {
+        return x;
+    }
+});
+
+console.log(numbersOdd);
